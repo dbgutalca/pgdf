053cd9eeac04a152c549e2837550a1562f7540dc
Sebastián Ferrada <scferradaa@gmail.com>
2023-11-14 12:50:45 -0300

    adds bash script for experiments

---
+++ experiments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run the script for each specified value
+for value in 0.1 1 10 100; do
+    
+    # Run the script
+    ./your_script.sh "$value"
+    
+    # Run jar for each file type
+    for file_type in pgdf yarspg graphml graphson json; do
+        java -jar your_jar_file.jar /path/to/json "--$file_type"
+    done
+    
+    # Delete folder
+    rm -rf "$folder"
+done
